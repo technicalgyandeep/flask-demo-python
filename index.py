f14ecb06688df9e26ce464385acbb05754797812
technicalgyandeep <technoschool5218@gmail.com>
2020-09-17 17:59:10 +0530

    initial

---
+++ index.py
@@ -0,0 +1,11 @@
+from flask import Flask
+ 
+ app = flask(__name__)
+ 
+ @app.route('/')
+ 
+ def index():
+     return "Hello from the Server"
+     
+ if __name__ == '__main__':
+      app.run(debug
